e62a6309e3da132ee68aff25617aaccdf55315e5
choiseyeon <choi20014830@gmail.com>
2023-03-02 22:58:08 +0900

    feat: 핸들러 방식 변경 및 Negroni 미들웨어 추가

--- main.go
+++ main.go
@@ -1,33 +1,13 @@
 package main
 
 import (
+	"github.com/codegangsta/negroni"
+	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 )
 
-func main() {
-	world := newWorld()
-	go world.run()
-
-	http.HandleFunc("/", serveHome)
-
-	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		client := NewClient(world, conn)
-		client.world.ChanEnter <- client
-	})
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-}
-
-func serveHome(w http.ResponseWriter, r *http.Request) {
+func serveHome(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	log.Println(r.URL)
 	if r.URL.Path != "/" {
 		http.Error(w, "Not found", http.StatusNotFound)
@@ -38,4 +18,34 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.ServeFile(w, r, "frontend/home.html")
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", serveHome)
+
+	n := negroni.Classic()
+	n.UseHandler(router)
+
+	n.Run(":8080")
+	
+	//world := newWorld()
+	//go world.run()
+
+	//http.HandleFunc("/", serveHome)
+	//
+	//http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
+	//	conn, err := upgrader.Upgrade(w, r, nil)
+	//	if err != nil {
+	//		log.Println(err)
+	//		return
+	//	}
+	//	client := NewClient(world, conn)
+	//	client.world.ChanEnter <- client
+	//})
+	//
+	//err := http.ListenAndServe(":8080", nil)
+	//if err != nil {
+	//	log.Fatal("ListenAndServe: ", err)
+	//}
 }
